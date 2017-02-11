02d8a5ccc2ae80e0de6011821f74b1df3b7f0399
Katrin Lasberg <klasberg@itcollege.ee>
2017-02-11 16:58:08 +0200

    Esimene commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+		<meta charset="utf-8">
+		<title>klasberg</title>
+	</head>
+	<body>
+		<h1>Suvaline pilt internetist</h1>
+		<p> Kas nyyd t88tab?</p>
+		<img src="suva.jpg" alt="mootorratas" style=100%/>
+	</body>
+</html>
