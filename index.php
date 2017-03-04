78a94289074901780b6884c42364d8b53e10ec9a
Katrin Lasberg <klasberg@itcollege.ee>
2017-03-04 16:35:16 +0200

    teine praks

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html>
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);?>
+
 	<head>
 	<link rel="stylesheet" type="text/css" href="style.css">
 		<meta charset="utf-8">
@@ -7,7 +11,15 @@
 	</head>
 	<body>
 		<h1>Suvaline pilt internetist</h1>
+		
 		<p> Kas nyyd t88tab?</p>
 		<img src="suva.jpg" alt="mootorratas" style=100%/>
+		<div id="kuupaev"><script src="plugin.js"></script></div>
+		<?php
+		echo 'Current PHP version: ' . phpversion();
+		echo phpversion('tidy');
+		?>
 	</body>
 </html>
+
+
